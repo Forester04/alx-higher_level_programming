615ed2116d9a36690f78b47881c734769c9d2182
Forester Erick <erickforester2018@gmail.com>
2023-11-14 03:47:08 +0300

    using process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv.length === 0) { console.log('No Argument'); } else if (argv.length === 1) { console.log('Argument found'); } else { console.log('Arguments found'); }
+if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found”'); }
