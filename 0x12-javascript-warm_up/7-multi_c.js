a503199d649fcc83c7504dee6d8d7c3e8fc833c3
Forester Erick <erickforester2018@gmail.com>
2023-11-14 16:10:41 +0300

    review of 7-multi code

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,5 @@
-!/usr/bin/node
+#!/usr/bin/node
 const { argv } = require('process');
 const occurence = Number(argv[2]);
-const display = () => {
-  for (let i = 0; i < occurence; i++) {
-    console.log('C is fun');
-  }
-};
-isNaN(occurence)
-  ? (console.log('Missing number of occurrences'))
-  : (display());
+if (isNaN(occurence)) { console.log('Missing number of occurrences'); } else { for (let i = 0; i < occurence; i++) { console.log('C is fun'); } }
+
